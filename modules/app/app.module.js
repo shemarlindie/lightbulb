34245b4db69b5dd180f4484305b8947db0decff8
Shemar Lindie <shemarlindie@gmail.com>
2015-11-29 20:01:49 -0400

    Add preloader. Fix text overlapping on toolbar.

--- modules/app/app.module.js
+++ modules/app/app.module.js
@@ -3,6 +3,13 @@
   'use strict';
 
   // MODULE DEFINITION
-  angular.module('app', ['app.api', 'app.appdea', 'ui.router', 'ngMaterial', 'ngAnimate', 'ngAria', 'ngSanitize']);
-  
+  angular.module('app', ['app.api', 'app.appdea', 'ui.router', 'ngMaterial', 'ngAnimate', 'ngAria', 'ngSanitize'])
+
+    .run(function () {
+      // remove preloader
+      $('#preloader').fadeOut(500, function () {
+        $(this).remove();
+      });
+    });
+
 })(angular);
